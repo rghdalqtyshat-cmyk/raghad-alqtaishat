4128ad4261786dddc195b84bfc4c45e2a7feaf22
raghad alqtaishat <rghdalqtyshat@gmail.com>
2025-09-28 13:32:21 +0300

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,29 @@
+num1=int(input("enter num 1:"))
+num2=int(input("enter num 2:"))
+print("sum=:",num1+num2)
+print("diffrence:",num1-num2)
+print("multification:",num1*num2)
+print("division=:",num1/num2)
+print("remainder=:",num1/num2)
+print("power=:",num1**num2)
+print("floor division=:",num1//num2)
+
+
+
+
+grade=int(input("enter your grade:"))
+if grade>80:
+    print("c-")
+elif grade>65:
+    print("d")
+else:
+ print("على الله")
+
+#10-30          50-60
+x=int(input("enter the value of x:"))
+if x in range(10,30):
+   print("يا سلام ")
+elif x in range(50,60):
+ print("فالح")
+else:
+   print("namastee")
